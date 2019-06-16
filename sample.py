c46c63fb217d45ca88cbe78a37c8762067506761
Apurva Patel <airplaneap@gmail.com>
2019-06-15 21:04:22 -0500

    Add init sample code
    remove sample model temporarily due to git lfs restrictions

---
+++ sample.py
@@ -0,0 +1,15 @@
+from imageai.Detection import ObjectDetection
+import os
+
+execution_path = os.getcwd()
+
+detector = ObjectDetection()
+detector.setModelTypeAsRetinaNet()
+detector.setModelPath(os.path.join(
+    execution_path, "models/resnet50_coco_best_v2.0.1.h5"))
+detector.loadModel()
+detections = detector.detectObjectsFromImage(input_image=os.path.join(
+    execution_path, "./images/image1.jpg"), output_image_path=os.path.join(execution_path, "imagenew.jpg"))
+
+for eachObject in detections:
+    print(eachObject["name"], " : ", eachObject["percentage_probability"])
